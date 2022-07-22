84638e35ed8a11e502d145702b4e886ac426a949
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-07-22 09:54:01 +0800

    update file

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,8 +1,14 @@
+// 該文件是整個項目的入口文件
+// 引入Vue
 import Vue from 'vue'
+// 引入App組件，它是所有組件的父組件
 import App from './App.vue'
-
+// 關閉vue的生產提示
 Vue.config.productionTip = false
 
+// 創建Vue實例對象--vm
 new Vue({
+  el: '#app',
+  // 將App組件放入容器中
   render: h => h(App),
-}).$mount('#app')
+})
