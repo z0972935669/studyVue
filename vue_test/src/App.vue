e20d3c9c6ce1b487283f340ffbeb8f0182833178
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-07-25 13:49:54 +0800

    update file

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,21 +1,73 @@
 <template>
-    <div>
-        <School/>
-        <hr>
-        <Student/>
+  <div id="root">
+    <div class="todo-container">
+        <div class="todo-wrap">
+            <MyHeader />
+            <MyList />
+            <MyFooter />
+        </div>
     </div>
+  </div>
 </template>
 
 <script>
-import School from './components/School'
-import Student from './components/Student'
+import MyHeader from "./components/MyHeader";
+import MyList from "./components/MyList";
+import MyFooter from "./components/MyFooter";
 
 export default {
-    name: 'App',
-    components:{School, Student},
-}
+  name: "App",
+  components: {
+    MyHeader,
+    MyList,
+    MyFooter,
+  },
+};
 </script>
 
 <style>
+/* base */
+body {
+  background: #fff;
+}
 
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
 </style>
---
+++ vue_test/src/components/MyItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <li>
+    <label for="">
+      <input type="checkbox" />
+      <span>xxxxx</span>
+    </label>
+    <button class="btn btn-danger" style="display: none">刪除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "MyItem",
+};
+</script>
+
+<style scoped>
+/* item */
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+</style>
