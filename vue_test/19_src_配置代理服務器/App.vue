c9c897d4e617ab25baae585df8b96422304c5e88
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-07-29 17:28:21 +0800

    update file

---
+++ vue_test/19_src_配置代理服務器/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button @click="getStudents">獲取學生信息</button>
+    <button @click="getCars">獲取汽車信息</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "App",
+  methods: {
+    getStudents() {
+      axios.get('http://localhost:8080/api/students').then(
+        response => {
+          console.log('請求成功了', response.data);
+        },
+        error => {
+          console.log('請求失敗了', error.message);
+        }
+      )
+    },
+    getCars() {
+      axios.get('http://localhost:8080/demo/cars').then(
+        response => {
+          console.log('請求成功了', response.data);
+        },
+        error => {
+          console.log('請求失敗了', error.message);
+        }
+      )
+    }
+  },
+};
+</script>
--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,36 +1,21 @@
 <template>
-  <div>
-    <button @click="getStudents">獲取學生信息</button>
-    <button @click="getCars">獲取汽車信息</button>
+  <div id="app">
+    <div class="container">
+      <Search />
+      <List />
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import Search from './components/Search'
+import List from './components/List'
 
 export default {
   name: "App",
-  methods: {
-    getStudents() {
-      axios.get('http://localhost:8080/api/students').then(
-        response => {
-          console.log('請求成功了', response.data);
-        },
-        error => {
-          console.log('請求失敗了', error.message);
-        }
-      )
-    },
-    getCars() {
-      axios.get('http://localhost:8080/demo/cars').then(
-        response => {
-          console.log('請求成功了', response.data);
-        },
-        error => {
-          console.log('請求失敗了', error.message);
-        }
-      )
-    }
-  },
+  components: {
+    Search,
+    List
+  }
 };
 </script>
