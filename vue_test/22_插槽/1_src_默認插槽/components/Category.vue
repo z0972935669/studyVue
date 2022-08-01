8f85002f7ef750aece11e8c77684e715f557887d
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-08-01 14:10:45 +0800

    update file

---
+++ vue_test/22_插槽/1_src_默認插槽/components/Category.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分類</h3>
+    <!-- 定義一個插槽 (挖坑，等著組件的使用者進行填充) -->
+    <slot>我是一些默認值，當使用者沒有傳遞具體結構時，我會出現</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ["title"],
+};
+</script>
+
+<style>
+.category {
+  background: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background: orange;
+}
+</style>
