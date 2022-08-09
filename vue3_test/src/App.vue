6d68dceb939edba15e1be8d51dfe1045551b6f72
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-08-09 15:06:42 +0800

    update file

--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>一個人的信息</h1>
-  <h2>姓名: {{ person.name }}</h2>
+  <h2 v-show="person.name">姓名: {{ person.name }}</h2>
   <h2>年齡: {{ person.age }}</h2>
+  <h2 v-show="person.sex">性別: {{ person.sex }}</h2>
   <h3>工作種類: {{ person.job.type }}</h3>
   <h3>工作薪水: {{ person.job.salary }}</h3>
   <h3>愛好: {{ person.hobby }}</h3>
   <h3>測試的數據c: {{ person.job.a.b.c }}</h3>
   <button @click="changeInfo">修改人的信息</button>
+  <button @click="addSex">添加一個sex屬性</button>
+  <button @click="deleteName">刪除一個name屬性</button>
 </template>
 
 <script>
@@ -40,11 +43,19 @@ export default {
       person.hobby[0] = "學習";
       // console.log(job);
     }
+    function addSex() {
+      person.sex = '男'
+    }
+    function deleteName() {
+      delete person.name
+    }
 
     // 返回一個對象(常用)
     return {
       changeInfo,
       person,
+      addSex,
+      deleteName,
     };
   },
 };
