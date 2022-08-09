523114ef548d512ede9210c3448c5a86fd5e8a68
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-08-09 11:26:41 +0800

    update file

---
+++ vue3_test/03_src_ref函數/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>一個人的信息</h1>
+  <h2>姓名: {{name}}</h2>
+  <h2>年齡: {{age}}</h2>
+  <h3>工作種類: {{job.type}}</h3>
+  <h3>工作薪水: {{job.salary}}</h3>
+  <button @click="changeInfo">修改人的信息</button>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    // 數據
+    let name = ref('張三')
+    let age = ref(18)
+    let job = ref({
+      type: '前端工程師',
+      salary: '30K'
+    })
+
+    // 方法
+    function changeInfo(){
+      // name.value = '李四'
+      // age.value = 48
+      job.value.type = 'UI設計師'
+      job.value.salary = '60K'
+      // console.log(name,age,job);
+    }
+
+    // 返回一個對象(常用) 
+    return {
+      name,
+      age,
+      job,
+      changeInfo
+    }
+  }
+}
+</script>
--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,71 +1,51 @@
 <template>
   <h1>一個人的信息</h1>
-  <h2>姓名: {{name}}</h2>
-  <h2>年齡: {{age}}</h2>
-  <h2>性別: {{sex}}</h2>
-  <h2>a的值是: {{a}}</h2>
-  <button @click="sayHello">說話(Vue3所配置的-sayHello)</button>
-  <br><br>
-  <button @click="sayWelcome">說話(Vue2所配置的-sayWelcome)</button>
-  <br><br>
-  <button @click="test1">測試一下在Vue2的配置中去讀取Vue3中的數據、方法</button>
-  <br><br>
-  <button @click="test2">測試一下在Vue3的setup配置中去讀取Vue2中的數據、方法</button>
+  <h2>姓名: {{ person.name }}</h2>
+  <h2>年齡: {{ person.age }}</h2>
+  <h3>工作種類: {{ person.job.type }}</h3>
+  <h3>工作薪水: {{ person.job.salary }}</h3>
+  <h3>愛好: {{ person.hobby }}</h3>
+  <h3>測試的數據c: {{ person.job.a.b.c }}</h3>
+  <button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
-import {h} from 'vue'
-
+import { reactive } from "vue";
 export default {
-  name: 'App',
-  data() {
-    return {
-      sex: '男',
-      a: 100
-    }
-  },
-  methods: {
-    sayWelcome() {
-      alert('歡迎來到尚硅谷學習')
-    },
-    test1() {
-      console.log(this.sex);
-      console.log(this.name);
-      console.log(this.age);
-      console.log(this.sayHello);
-    }
-  },
-  // 此處只是測試一下setup,暫時不考慮響應式的問題
+  name: "App",
   setup() {
     // 數據
-    let name = '張三'
-    let age = 18
-    let a = 200
+    let person = reactive({
+      name: "張三",
+      age: 18,
+      job: {
+        type: "前端工程師",
+        salary: "30K",
+        a: {
+          b: {
+            c: 666,
+          },
+        },
+      },
+      hobby: ["抽煙", "喝酒", "睡覺"],
+    });
 
     // 方法
-    function sayHello(){
-      alert(`我叫${name}，我${age}歲了，你好啊!`)
+    function changeInfo() {
+      person.name = "李四";
+      person.age = 48;
+      person.job.type = "UI設計師";
+      person.job.salary = "60K";
+      person.job.a.b.c = 999;
+      person.hobby[0] = "學習";
+      // console.log(job);
     }
 
-    function test2() {
-      console.log(name);
-      console.log(age);
-      console.log(sayHello);
-      console.log(this.sex);
-      console.log(this.sayWelcome);
-    }
-
-    // 返回一個對象(常用) 
+    // 返回一個對象(常用)
     return {
-      name,
-      age,
-      sayHello,
-      test2,
-      a
-    }
-
-    // 返回一個函數(渲染函數)
-    // return () => h('h1','尚硅谷')
-  }
-}
+      changeInfo,
+      person,
+    };
+  },
+};
 </script>
