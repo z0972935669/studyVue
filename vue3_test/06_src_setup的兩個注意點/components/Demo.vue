f06d694fedc7c3f6eea59d9ce572a4d0057f8eba
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-08-09 16:20:44 +0800

    update file

---
+++ vue3_test/06_src_setup的兩個注意點/components/Demo.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>一個人的信息</h1>
+  <h2 v-show="person.name">姓名: {{ person.name }}</h2>
+  <h2>年齡: {{ person.age }}</h2>
+  <button @click="test">測試觸發一下Demo組件的Hello事件</button>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "Demo",
+  props: ["msg", "school"],
+  emits:['hello'],
+  setup(props, context) {
+    // console.log('---setup---',props);
+    // console.log('---setup---',context.attrs); //相當於Vue2中的$attrs
+    // console.log("---setup---", context.emit); //觸發自定義事件的
+    console.log("---setup---", context.slots); //插槽
+    // 數據
+    let person = reactive({
+      name: "張三",
+      age: 18,
+    });
+
+    // 方法
+    function test() {
+        context.emit('hello', 666)
+    }
+
+    // 返回一個對象(常用)
+    return {
+      person,
+      test
+    };
+  },
+};
+</script>
