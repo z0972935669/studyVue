d055b9ae24ab3d656a8737ca49dc420c1bb69a20
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-08-11 17:30:54 +0800

    update file

---
+++ vue3_test/20_src_Teleport/components/Dialog.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click="isShow = true">點我彈個窗</button>
+    <!-- to:可以寫html元素，html,body等等都可以... -->
+    <teleport to="body">
+        <div v-if="isShow" class="mask">
+            <div class="dialog">
+                <h3>我是一個彈窗</h3>
+                <h4>一些內容</h4>
+                <h4>一些內容</h4>
+                <h4>一些內容</h4>
+                <button @click="isShow = false">關閉彈窗</button>
+            </div>
+        </div>
+    </teleport>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+    name: 'Dialog',
+    setup() {
+        let isShow = ref(false)
+        return {
+            isShow
+        }
+    }
+}
+</script>
+
+<style>
+.mask {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0,0,0,0.5);
+}
+.dialog{
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 300px;
+    height: 300px;
+    background: green;
+}
+</style>
---
+++ vue3_test/src/components/Child.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="child">
+    <h3>我是Child組件</h3>
+    <Son/>
+  </div>
+</template>
+
+<script>
+import Son from './Son'
+
+export default {
+  name: 'Child',
+  components: {
+    Son
+  },
+}
+</script>
+
+<style>
+.child {
+  background: skyblue;
+  padding: 10px;
+}
+</style>
