59857439c7fb313d390a2c32b9d24479ca74a832
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-08-11 15:45:42 +0800

    update file

---
+++ vue3_test/18_src_provide與inject/components/Son.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="son">
+    <h3>我是Son組件(孫),{{car.name}}--{{car.price}}</h3>
+  </div>
+</template>
+
+<script>
+import {inject} from 'vue'
+export default {
+  name: 'Son',
+  setup() {
+    let car = inject('car')
+    console.log(car, '****');
+    return {car}
+  }
+}
+</script>
+
+<style>
+.son {
+  background: orange;
+  padding: 10px;
+}
+</style>
