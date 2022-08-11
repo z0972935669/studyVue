0e6df00e3df257425e573ad16c919f4503834a0a
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-08-11 10:55:41 +0800

    update file

---
+++ vue3_test/15_src_readonly與shallowReadonly/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="isShowDemo = !isShowDemo">切換隱藏/顯示</button>
+  <Demo v-if="isShowDemo"/>
+</template>
+
+<script>
+import {ref} from 'vue'
+import Demo from './components/Demo';
+export default {
+  name: "App",
+  components: {
+    Demo,
+  },
+  setup() {
+    let isShowDemo = ref(true)
+    return {isShowDemo}
+  }
+};
+</script>
