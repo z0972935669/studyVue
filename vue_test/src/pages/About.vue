48b3ce553638ea2948c5372d0ff8f5819ffd92d3
z0972935669 <76981997+z0972935669@users.noreply.github.com>
2022-10-23 18:18:29 +0800

    update fille

---
+++ vue_test/src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>我是About的内容</h2>
+</template>
+
+<script>
+export default {
+    name: 'About',
+    
+
+    // mounted() {
+    //   console.log('About組件掛載完畢了',this);
+    //   window.aboutRoute = this.$route
+    //   window.aboutRouter = this.$router
+    // },
+}
+</script>
